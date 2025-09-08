ba5240c4fa74533f86d9bb5ba93a50d72dc7645d
marianahovanec-dev <marianahovanec@gmail.com>
2025-09-08 12:17:43 +0300

    коментар

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, b, c, r, S, P;
+
+    cout << "Введіть a, b, c: " << endl;
+    cin >> a >> b >> c;
+
+    // Периметр 
+    P = a + b + c;
+
+    // Напівпериметр
+    S = P / 2;
+
+    // Площа за формулою Герона
+    r = sqrt(S * (S - a) * (S - b) * (S - c));
+
+    cout << "Периметр = " << P << endl;
+    cout << "Півпериметр = " << S << endl;
+    cout << "Площа = " << r << endl;
+
+    return 0;
+}
